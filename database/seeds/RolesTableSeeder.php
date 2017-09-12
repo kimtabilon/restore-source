3f7f7d9a4f851b4efcdfe0bae08b5febde5a7c32
kim.tabilon <kim.tabilon12345@gmail.com>
2017-09-12 13:51:38 -0700

    Fixed migration issue and seeder for users also created.

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -12,9 +12,35 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Role::class)->create(['name' => 'Cashier']);
-        factory(App\Role::class)->create(['name' => 'Receiving Coordinator']);
-        $manager = factory(App\Role::class)->create(['name' => 'Manager']);
-        factory(App\User::class)->create(['role_id' => $manager->id]);
+        $cashier    = factory(App\Role::class)->create(['name' => 'Cashier']);
+        $rc         = factory(App\Role::class)->create(['name' => 'Receiving Coordinator']);
+        $manager    = factory(App\Role::class)->create(['name' => 'Manager']);
+        
+        factory(App\User::class)
+            ->create([
+                'role_id'       => $manager->id,
+                'given_name'    => 'Francis',
+                'middle_name'   => '',
+                'last_name'     => 'Macatulad',
+                'email'         => 'francis.macatulad@habitat.org.ph',
+                ]);
+            
+        factory(App\User::class)
+            ->create([
+                'role_id'       => $rc->id,
+                'given_name'    => 'Jericho',
+                'middle_name'   => '',
+                'last_name'     => 'Vasquez',
+                'email'         => 'jericho.vasquez@habitat.org.ph',
+                ]);
+                
+        factory(App\User::class)
+            ->create([
+                'role_id'       => $cashier->id,
+                'given_name'    => 'Kem Robert',
+                'middle_name'   => '',
+                'last_name'     => 'Tan',
+                'email'         => 'kemrobert.tan@habitat.org.ph',
+                ]);            
     }
 }
